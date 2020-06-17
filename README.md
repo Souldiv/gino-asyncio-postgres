75b096617097a253537413d72156c491f1ff193c
Souldiv <amrut.phadke2016@vitstudent.ac.in>
2020-06-17 18:32:28 +0530

    requirements and readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# gino-asyncio-postgres
+
+## Setup dev environment
+1. pip install -r requirements.txt
+2. python >= 3.5
+```
+python server.py
+```
--- create_db.py
+++ create_db.py
@@ -3,6 +3,7 @@ import asyncio
 from sys import platform
 from gino.ext.sanic import Gino
 from config import *
+from sanic_cors import CORS
 
 
 app = Sanic("Heros legacy")
@@ -11,5 +12,6 @@ app.config.DB_DATABASE = DB_ARGS['database']
 app.config.DB_PASSWORD = DB_ARGS['password']
 app.config.DB_USER = DB_ARGS['user']
 app.config.DB_PORT = DB_ARGS['port']
+CORS(app, resources={r"/*": {"origins": "*"}})
 db = Gino()
 db.init_app(app)
